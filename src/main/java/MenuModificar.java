b6cd6f96bab490cee7becf7e00a196ea85e3d61a
Eloy Jiménez Medina <ejm0005@alu.medac.es>
2026-05-06 14:21:03 +0200

    Creado el main menu junto con sus botones y labels. Se han enlazado los botones a las clases que deben ejecutar

--- src/main/java/MenuModificar.java
+++ src/main/java/MenuModificar.java
@@ -1,2 +1,37 @@
-public class MenuModificar {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class MenuModificar extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MenuImprimir frame = new MenuImprimir();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public MenuModificar() {
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+    }
+
 }

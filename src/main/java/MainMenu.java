3b98d4cca219d30da501b38d178ea55f3cde60fc
Eloy Jiménez Medina <ejm0005@alu.medac.es>
2026-05-12 09:19:29 +0200

    Reordenación de codigo y eliminación de importaciones sin uso

--- src/main/java/MainMenu.java
+++ src/main/java/MainMenu.java
@@ -1,9 +1,15 @@
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.EventQueue;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JSeparator;
 
 public class MainMenu extends JFrame {
 
@@ -37,9 +43,29 @@ public class MainMenu extends JFrame {
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
         setContentPane(contentPane);
 
+        //Título interno del programa
+        JLabel title = new JLabel("Elditrón S.L.");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setFont(new Font("Segoe UI Black", Font.PLAIN, 22));
+        title.setBounds(137, 20, 162, 48);
+        contentPane.add(title);
+
+        //Separador de contenidos
+        JSeparator separator = new JSeparator();
+        separator.setBounds(53, 78, 330, 2);
+        contentPane.add(separator);
+
+        //Label que indica al usuario que debe seleccionar una opción
+        JLabel lblSeleccioneLaOpcin = new JLabel("Seleccione la opción que desea realizar");
+        lblSeleccioneLaOpcin.setHorizontalAlignment(SwingConstants.CENTER);
+        lblSeleccioneLaOpcin.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+        lblSeleccioneLaOpcin.setBounds(10, 90, 416, 24);
+        contentPane.add(lblSeleccioneLaOpcin);
+
+        //Botón que lanza el menu de insertar registros
         JButton MenuInsertarRegistro = new JButton("Insertar registro");
         MenuInsertarRegistro.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 14));
-        MenuInsertarRegistro.setBounds(96, 109, 249, 32);
+        MenuInsertarRegistro.setBounds(96, 124, 249, 32);
         MenuInsertarRegistro.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 MenuInsertar insertar = new MenuInsertar();
@@ -49,6 +75,7 @@ public class MainMenu extends JFrame {
         contentPane.setLayout(null);
         contentPane.add(MenuInsertarRegistro);
 
+        //Botón que lanza el menu de consultar registros
         JButton MenuConsultarRegistro = new JButton("Consultar registro");
         MenuConsultarRegistro.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 14));
         MenuConsultarRegistro.addActionListener(new ActionListener() {
@@ -57,9 +84,10 @@ public class MainMenu extends JFrame {
                 imprimir.setVisible(true);
             }
         });
-        MenuConsultarRegistro.setBounds(96, 151, 249, 32);
+        MenuConsultarRegistro.setBounds(96, 166, 249, 32);
         contentPane.add(MenuConsultarRegistro);
 
+        //Botón que lanza el menu de modificar registros
         JButton MenuModificarRegistro = new JButton("Modificar registro");
         MenuModificarRegistro.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 14));
         MenuModificarRegistro.addActionListener(new ActionListener() {
@@ -68,24 +96,9 @@ public class MainMenu extends JFrame {
                 modificar.setVisible(true);
             }
         });
-        MenuModificarRegistro.setBounds(96, 193, 249, 32);
+        MenuModificarRegistro.setBounds(96, 208, 249, 32);
         contentPane.add(MenuModificarRegistro);
 
-        JLabel lblNewLabel = new JLabel("Elditrón S.L.");
-        lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        lblNewLabel.setFont(new Font("Segoe UI Black", Font.PLAIN, 22));
-        lblNewLabel.setBounds(140, 10, 162, 48);
-        contentPane.add(lblNewLabel);
-
-        JLabel lblSeleccioneLaOpcin = new JLabel("Seleccione la opción que desea realizar");
-        lblSeleccioneLaOpcin.setHorizontalAlignment(SwingConstants.CENTER);
-        lblSeleccioneLaOpcin.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-        lblSeleccioneLaOpcin.setBounds(10, 68, 416, 24);
-        contentPane.add(lblSeleccioneLaOpcin);
-
-        JSeparator separator = new JSeparator();
-        separator.setBounds(49, 57, 330, 2);
-        contentPane.add(separator);
 
     }
 }

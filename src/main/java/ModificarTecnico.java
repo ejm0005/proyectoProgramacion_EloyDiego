5fb7f384d1b5d623797d0efd8eb3f68882ce98bb
Eloy Jiménez Medina <ejm0005@alu.medac.es>
2026-05-12 10:27:54 +0200

    Creación del menu modificar y agregación de codigo base a las clases ModificarCliente/Encargo/Tecnico

--- src/main/java/ModificarTecnico.java
+++ src/main/java/ModificarTecnico.java
@@ -1,2 +1,38 @@
-public class ModificarTecnico {
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class ModificarTecnico extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    ModificarTecnico frame = new ModificarTecnico();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public ModificarTecnico() {
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+
+    }
+
 }
